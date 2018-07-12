d2d5d85008bbc739ef03d2e7485d0eeb789d22f4
dacremann <dacremann@xebialabs.com>
2018-07-12 16:24:17 +0200

    ceci est la version 1.1

--- mytemplate.groovy
+++ mytemplate.groovy
@@ -1,6 +1,6 @@
-// Exported from:        http://pc13.home:5516/#/templates/Folder935e868f746c45faa6d82e0138ee16be-Releaseec0d2dbe4534ccba6e7ed422107a162/releasefile
+// Exported from:        http://MacBook-Pro-de-David.local:5516/#/templates/Folder935e868f746c45faa6d82e0138ee16be-Releaseec0d2dbe4534ccba6e7ed422107a162/releasefile
 // XL Release version:   8.1.0
-// Date created:         Wed Jul 11 20:10:30 CEST 2018
+// Date created:         Thu Jul 12 16:24:16 CEST 2018
 
 xlr {
   template('MyTemplate') {
